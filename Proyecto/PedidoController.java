4d344fb9f7a7006a1ef71334b540788035f9701d
Fernanda De la Cruz <147539828+Fer2507@users.noreply.github.com>
2026-08-20 21:19:43 -0700

    Add files via upload

---
+++ Proyecto/PedidoController.java
@@ -0,0 +1,10 @@
+package itch.dlc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/pedido")
+public class PedidoController {
+
+}
